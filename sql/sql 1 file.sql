3820238d252c21183905d6a57d046fc4d47dd219
JakiroDust <20521414@gm.uit.edu.vn>
2022-06-03 16:26:59 +0700

    Update sql 1 file.sql

--- sql/sql 1 file.sql
+++ sql/sql 1 file.sql
@@ -1,6 +1,6 @@
-﻿CREATE DATABASE QuanLyKhachSan
-GO
-
+﻿go
+CREATE DATABASE QuanLyKhachSan
+go
 USE QuanLyKhachSan
 GO
 
@@ -496,22 +496,23 @@ end
 end
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 /**********/
-INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) SELECT 'A',150000;
-INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) SELECT 'B',170000;
-INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) SELECT 'C',200000;
-INSERT INTO LOAIKHACH (LoaiKhach,HeSo) SELECT 'KHÁCH THƯỜNG',1;
-INSERT INTO LOAIKHACH (LoaiKhach,HeSo) SELECT 'NGOẠI QUỐC',1.5;
-INSERT INTO THAMSO (TenThamSo,GiaTriThamSo) SELECT 'SoKhachToiDa',3;
-insert into PHUTHU(SoLuongKhach,PhuThu) select 3,1.25;
+Declare @A int, @B int, @C int,@P1 int, @P2 int ,@P3 int, @P4 int, @P5 int, @P6 int
+INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) values('A',150000);
+INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) values('B',170000);
+INSERT INTO LOAIPHONG (TenLoaiPhong,DonGia) values('C',200000);
+INSERT INTO LOAIKHACH (LoaiKhach,HeSo) values (N'KHÁCH THƯỜNG',1);
+INSERT INTO LOAIKHACH (LoaiKhach,HeSo) values (N'NGOẠI QUỐC',1.5);
+INSERT INTO THAMSO (TenThamSo,GiaTriThamSo) values ('SoKhachToiDa',3);
+insert into PHUTHU(SoLuongKhach,PhuThu) values (3,1.25);
 select @A=LOAIPHONG.MaLoaiPhong from LOAIPHONG where LOAIPHONG.TenLoaiPhong='A'
 select @B=LOAIPHONG.MaLoaiPhong from LOAIPHONG where LOAIPHONG.TenLoaiPhong='B'
 select @C=LOAIPHONG.MaLoaiPhong from LOAIPHONG where LOAIPHONG.TenLoaiPhong='C'
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 1,@A,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 2,@A,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 3,@A,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 4,@B,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 5,@B,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 6,@B,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 7,@C,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 8,@C,1;
-INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) SELECT 9,@C,1;
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (1,@A,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (2,@A,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (3,@A,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (4,@B,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (5,@B,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (6,@B,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (7,@C,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (8,@C,1);
+INSERT INTO PHONG (TenPhong,MaLoaiPhong,TinhTrang) values (9,@C,1);
